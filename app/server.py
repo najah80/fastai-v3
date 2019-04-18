5d03032396231bedf1fd51c0699b517d66d6a46b
NAJAH HATMI <46248928+najah80@users.noreply.github.com>
2019-04-18 08:41:20 +0100

    Update server.py

--- app/server.py
+++ app/server.py
@@ -8,8 +8,7 @@ from io import BytesIO
 from fastai import *
 from fastai.vision import *
 
-# export_file_url = 'https://www.dropbox.com/s/v6cuuvddq73d1e0/export.pkl?raw=1'
-export_file_url = 'https://www.dropbox.com/s/6bgq8t6yextloqp/export.pkl?raw=1'
+export_file_url = 'https://drive.google.com/uc?export=download&id=1-4uh5LpjUA1zWYQnDEOmRoobj_95AMCM'
 export_file_name = 'export.pkl'
 
 classes = ['tunisian', 'french']
